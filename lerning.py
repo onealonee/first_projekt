403afff5b700e6017a31e31ca8baa10f0a79c189
onealonee <kazimiov.sk.xx@mail.ru>
2023-09-14 16:48:52 +0300

    Update lerning.py

--- lerning.py
+++ lerning.py
@@ -1,11 +1,16 @@
-def quick_merge(n):
-    result = []
-    a = []
-    for i in range(n):
-        a = [int(c) for c in input().split()]
-        result = result + a
-        result.sort()
-    return (result)
-numbers1 = int(input())
-print(*(quick_merge(numbers1)))
+# объявление функции
+def is_password_good(password):
+    c = 0
+    if len(password) > 8:
+        for i in range(len(password)):
+            if password[i].upper() == password[i]:
+                c += 1
+            elif password[i].lower() == password[i]:
+                c += 1
+            elif i 
 
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(is_password_good(txt))
